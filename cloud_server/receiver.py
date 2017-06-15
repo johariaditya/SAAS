596ab90441fba79e45c4519fadf74562a307f7b9
root <root@localhost.localdomain>
2017-06-16 00:09:52 +0530

    receiver change

--- cloud_server/receiver.py
+++ cloud_server/receiver.py
@@ -3,13 +3,13 @@
 import socket,os,sys,commands
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(("",8888))
+while True:
+	cdata=s.recvfrom(100)
+	cdata1=s.recvfrom(100)
 
-cdata=s.recvfrom(100)
-cdata1=s.recvfrom(100)
-
-if cdata[0]=="test" and cdata1[0]=="123":
-	s.sendto("ok",cdata[1])
-else:
-	s.sendto("not ok",cdata[1])
+	if cdata[0]=="test" and cdata1[0]=="123":
+		s.sendto("ok",cdata[1])
+	else:
+		s.sendto("not ok",cdata[1])
 
 
